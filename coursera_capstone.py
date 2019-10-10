8b51c55e5ae40652ceac362947c69bf844c742d3
Grumpy-catzZ <44294632+Grumpy-catzZ@users.noreply.github.com>
2019-10-11 00:28:57 +0530

    Add files via upload

---
+++ coursera_capstone.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be used to complete the coursera capstone project- BATTLE OF NEIGHRBOURHOODS. Till now I have linked my hardware to github and have learnt few git codes :)
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
